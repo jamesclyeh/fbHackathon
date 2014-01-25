b0949cf69b0664cd502a2cf3e6e69c636542e65c
Alex Min <kyongtai.alex.min@gmail.com>
2014-01-24 21:09:02 -0500

    I suck at git lol

--- PebbleSDK-2.0-BETA7/facebookCheckIn/src/js/pebble-js-app.js
+++ PebbleSDK-2.0-BETA7/facebookCheckIn/src/js/pebble-js-app.js
@@ -10,11 +10,16 @@ function getLocationInfo(position) {
     var req = new XMLHttpRequest();
   	req.open('GET', 'https://graph.facebook.com/search?type=place&center=' + position.coords.latitude + ',' + position.coords.longitude + '&distance=10000&limit=2&access_token=CAACEdEose0cBAKuH9rUp2wGsK06TJNs6DNFjGgJY1NuVb3Ytjhm3WnQaJdOvu6TZB6rIELQySINLryLTL4f930GD8mhqokZAL82XZBWSZBCbpnmqlZAiUsz2ksZCBLj03lvkWOc9eEu709iRXi604NuEmUWDuPLii4UGVbZBfZCaQy3SjLHqCw1LKjdh20r7ZACkGT9xIP0GFvQZDZD', true);
   	req.onload = function(e) {
-    if (req.readyState == 4 && req.status == 200) {
-        if(req.status == 200) {
-            var response = JSON.parse(req.responseText);
-        	    console.log(response);
-      	} else { console.log("Error"); }
+    	if (req.readyState == 4 && req.status == 200) {
+        	if(req.status == 200) {
+       			var response = JSON.parse(req.responseText);
+        			var list ={};
+					for (var venue in response["data"]) {
+						list[venue["id"]] = venue["name"];
+					}
+      		} else { 
+      			console.log("Error"); 
+      		}
     	}
  	}
 
